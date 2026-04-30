01bd9270689608a395aa2597934aed107b964c10
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-30 08:43:01 -0400

    Update __main__.py

--- src/vba_unit/__main__.py
+++ src/vba_unit/__main__.py
@@ -1,1 +1,7 @@
-
+def __main__() -> None:
+    # find vba source code files
+    # find test files
+    # foeach testfile that begins with Test
+    #    Execute each sub that begins with Test
+    # generate a test report
+    # generate coverage report
