ef02db0c5af0ca5ba525a150ab9c6bb50fa290c2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-04 11:16:13 -0400

    Update test_testrunner.py

--- tests/test_testrunner.py
+++ tests/test_testrunner.py
@@ -16,7 +16,7 @@ def test_main(mocker: MockerFixture) -> None:
     os.chdir("./tests")
     main()
     assert mock_print.call_count == 4
-    assert mock_print.call_args_list[0].args[0] == "\n--- VBA Test Report ---\n"
+    assert mock_print.call_args_list[0].args[0] == "\n--- VBA Test Report ---"
     assert mock_print.call_args_list[1].args[0] == "test"
     assert mock_print.call_args_list[2].args[0] == "test"
     assert mock_print.call_args_list[3].args[0] == (
