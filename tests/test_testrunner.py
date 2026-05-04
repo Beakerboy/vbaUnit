866967f1e6afe68d7ea21f6ed9f2ca95650cff0f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-04 11:14:28 -0400

    Update expected print call count in test

--- tests/test_testrunner.py
+++ tests/test_testrunner.py
@@ -15,7 +15,7 @@ def test_main(mocker: MockerFixture) -> None:
     )
     os.chdir("./tests")
     main()
-    assert mock_print.call_count == 3
+    assert mock_print.call_count == 4
     assert mock_print.call_args_list[0].args[0] == "test"
     assert mock_print.call_args_list[1].args[0] == "test"
     assert mock_print.call_args_list[2].args[0] == (
