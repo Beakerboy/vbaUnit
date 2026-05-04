2c12b062215c4a573a70ce7762c4a36ca09c5339
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-04 11:02:08 -0400

    Update test_testrunner.py

--- tests/test_testrunner.py
+++ tests/test_testrunner.py
@@ -2,8 +2,8 @@ import os
 from vba_unit.vba_test_runner import main
 from pytest_mock import MockerFixture
 
-
-def test_main(mocker: MockerFixture) -> None:
+@patch('builtins.print')
+def test_main(mocker: MockerFixture, mock_print: str) -> None:
     mocker.patch(
         "sys.argv",
         [
@@ -14,3 +14,4 @@ def test_main(mocker: MockerFixture) -> None:
     )
     os.chdir("./tests")
     main()
+    mock_print.assert_called_with("Test")
