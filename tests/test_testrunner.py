663485ebdec2a2e4064fc44efd782ea64cb45278
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-04 10:32:37 -0400

    Update test_testrunner.py

--- tests/test_testrunner.py
+++ tests/test_testrunner.py
@@ -1,3 +1,4 @@
+import os
 from vba_unit.vba_test_runner import run_tests
 from pytest_mock import MockerFixture
 
@@ -7,7 +8,9 @@ def test_main(mocker: MockerFixture) -> None:
         "sys.argv",
         [
             "vba_test_runner.py",
-            "./project",
+            "--project",
+            "VbaProject"
         ],
     )
-    run_tests()
+    os.chdir("./tests")
+    main()
