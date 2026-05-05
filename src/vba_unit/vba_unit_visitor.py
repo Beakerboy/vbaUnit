985b0a905411e47d23e9418a692b767c464c924c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-05 09:32:38 -0400

    Implement visited lines tracking in VbaUnitVisitor
    Added methods to track visited lines in the VBA unit visitor.

--- src/vba_unit/vba_unit_visitor.py
+++ src/vba_unit/vba_unit_visitor.py
@@ -1,3 +1,4 @@
+from antlr4 import ParseTree
 from antlr4_vba.vbaParser import vbaParser as Parser
 from pyvba_interpreter.vba_visitor import VbaVisitor
 from typing import TypeVar
@@ -8,6 +9,21 @@ T = TypeVar('T', bound='VbaUnitVisitor')
 
 
 class VbaUnitVisitor(VbaVisitor):
+
+    def __init__(self: T, table: SymbolTable) -> None:
+        self.visited_lines = set()
+        self.source_lines = source_lines # List of strings from the VBA file
+        super().__init__(table)
+    
+    def visit(self: T, tree: ParseTree):
+        if tree is not None:
+            # Get the starting line number from the context
+            # ANTLR line numbers are typically 1-indexed
+            line_num = tree.start.line
+            self.visited_lines.add(line_num)
+        
+        # Call the original visit to continue traversal
+        return super().visit(tree)
 
     def visitAssertStatement(                                      # noqa: N802
             self: T,
