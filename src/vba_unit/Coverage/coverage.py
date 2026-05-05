c33057bd8f1378efa9fe89d57c1586e9444ffd48
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-05 17:31:27 -0400

    Update coverage.py

--- src/vba_unit/Coverage/coverage.py
+++ src/vba_unit/Coverage/coverage.py
@@ -1,1 +1,13 @@
+import json
+import requests
+from typing import TypeVar
 
+
+T = TypeVar('T', bound='Coverage')
+
+
+class Coverage():
+    def submit_report(self: T) -> str:
+        report = self.generate_report()
+        response = requests.post(self.endpoint, files={'json_file': json.dumps(report)})
+        return response.json()
