1dc13bfc82a759fa853f6bd7385bc8da101f6e1b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-05 17:38:48 -0400

    Add repo method to GitRepo class

--- src/vba_unit/Coverage/git_repo.py
+++ src/vba_unit/Coverage/git_repo.py
@@ -5,4 +5,21 @@ T = TypeVar('T', bound='GitRep')
 
 
 class GitRepo():
-    pass
+    def repo(self: T) -> dict:
+        return {
+            "head": {
+                "id": self.commit_sha,
+                "author_name": self.author_name,
+                "author_email": self.author_email,
+                "committer_name": self.committer_name,
+                "committer_email": self.committer_email,
+                "message": self.message
+            },
+            "branch": self.branch,
+            "remotes": [
+                {
+                    "name": "origin",
+                    "url": self.remote_url
+                }
+            ]
+        }
