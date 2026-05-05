ab30e21fc559d1dabd323b587df72cd0715b3fba
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-05 11:28:10 -0400

    Change import from ParseTree to Tree

--- src/vba_unit/vba_unit_visitor.py
+++ src/vba_unit/vba_unit_visitor.py
@@ -1,4 +1,4 @@
-from antlr4 import ParseTree
+from antlr4 import Tree
 from antlr4_vba.vbaParser import vbaParser as Parser
 from pyvba_interpreter.symbol_table import SymbolTable
 from pyvba_interpreter.vba_visitor import VbaVisitor
