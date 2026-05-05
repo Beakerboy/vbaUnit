f888d0d8cbae8a6b10747416173ab690a59b6546
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-05 19:18:51 -0400

    Update github.py

--- src/vba_unit/Coverage/github.py
+++ src/vba_unit/Coverage/github.py
@@ -14,7 +14,10 @@ class Coveralls(GitRepo):
         full_ref = os.environ.get('GITHUB_REF', 'master')
         branch = full_ref.replace('refs/heads/', '').replace('refs/pull/', 'PR-')
         fmt = "%an%n%ae%n%cn%n%ce%n%s"
-        details = subprocess.check_output(
+        (self.author_name,
+         self.author_email,
+         self.comitter_name,
+         self.comitter_email) = subprocess.check_output(
             ["git", "log", "-1", f"--pretty=format:{fmt}", self.commit_sha],
             text=True
         ).splitlines()
