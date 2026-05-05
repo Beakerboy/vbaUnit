733e9a8758e0a940d5a030ad90929d820365b0e2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-05 15:39:17 -0400

    Update __main__.py

--- src/vba_unit/__main__.py
+++ src/vba_unit/__main__.py
@@ -4,6 +4,7 @@ import hashlib
 import json
 import os
 import requests
+import subprocess
 from antlr4 import FileStream, CommonTokenStream, ParseTreeWalker
 from antlr4_vba.vbaLexer import vbaLexer
 from antlr4_vba.vbaParser import vbaParser
