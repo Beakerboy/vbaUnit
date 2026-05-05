db2d8f34a050aa7e806c5e8c9720c41341e3e749
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-05 18:50:12 -0400

    Update github.py

--- src/vba_unit/Coverage/github.py
+++ src/vba_unit/Coverage/github.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from typing import TypeVar
 from vba_unit.Coverage.git_repo import GitRepo
 
@@ -11,9 +12,9 @@ class Coveralls(GitRepo):
         self.job_id = os.environ['GITHUB_RUN_ID']
         self.commit_sha = commit_sha = os.environ.get('GITHUB_SHA')
         full_ref = os.environ.get('GITHUB_REF', 'master')
-    branch = full_ref.replace('refs/heads/', '').replace('refs/pull/', 'PR-')
-    fmt = "%an%n%ae%n%cn%n%ce%n%s"
-    details = subprocess.check_output(
-        ["git", "log", "-1", f"--pretty=format:{fmt}", self.commit_sha],
-        text=True
-    ).splitlines()
+        branch = full_ref.replace('refs/heads/', '').replace('refs/pull/', 'PR-')
+        fmt = "%an%n%ae%n%cn%n%ce%n%s"
+        details = subprocess.check_output(
+            ["git", "log", "-1", f"--pretty=format:{fmt}", self.commit_sha],
+            text=True
+        ).splitlines()
