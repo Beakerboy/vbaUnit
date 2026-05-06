d838e04fb1f4a5dc82f943c631d99645992fc91a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-06 14:16:48 -0400

    Update coverage.py

--- src/vba_unit/Coverage/coverage.py
+++ src/vba_unit/Coverage/coverage.py
@@ -9,6 +9,11 @@ T = TypeVar('T', bound='Coverage')
 
 
 class Coverage():
+    """
+    Send a json string to a service provider. The implementing
+    class shall create the string from the data in CoverageTable
+    and the class implementing GitRepo.
+    """
     def __init__(self: T) -> None:
         self.endpoint = ''
         self.table: CoverageTable
