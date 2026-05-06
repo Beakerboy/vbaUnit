267aab05988cccca7e4fadd129a1d20165984b87
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-06 12:39:47 -0400

    Update cli.py

--- src/vba_unit/cli.py
+++ src/vba_unit/cli.py
@@ -8,7 +8,7 @@ from typing import TypeVar
 from vba_unit.Coverage.coverage_factory import CovFact
 from vba_unit.Coverage.git_factory import GitFact
 from vba_unit.Interpreter.coverage_table import CoverageTable
-from vba_unit.Interpreter.unit_listener import VbaUnitListener
+from vba_unit.Interpreter.vba_unit_listener import VbaUnitListener
 from vba_unit.Interpreter.vba_unit_visitor import VbaUnitVisitor
 from vba_unit.test_fail_exception import TestFailException
 
