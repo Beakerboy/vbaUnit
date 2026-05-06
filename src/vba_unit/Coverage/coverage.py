c11bafe06f8b0a071091bc6947c0617da5c61f0b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-05 22:28:11 -0400

    Format post request for better readability

--- src/vba_unit/Coverage/coverage.py
+++ src/vba_unit/Coverage/coverage.py
@@ -9,5 +9,6 @@ T = TypeVar('T', bound='Coverage')
 class Coverage():
     def submit_report(self: T) -> str:
         report = self.generate_report()
-        response = requests.post(self.endpoint, files={'json_file': json.dumps(report)})
+        response = requests.post(
+            self.endpoint, files={'json_file': json.dumps(report)})
         return response.json()
