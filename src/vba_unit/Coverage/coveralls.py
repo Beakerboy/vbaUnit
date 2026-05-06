4156b0f7876d82d96b68b7c0197049574b1cafff
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-06 06:50:01 -0400

    Update coveralls.py

--- src/vba_unit/Coverage/coveralls.py
+++ src/vba_unit/Coverage/coveralls.py
@@ -15,11 +15,27 @@ class Coveralls(Coverage):
         self.git = git
 
     def coveralls_report(self: T) -> str:
-        file_path = "src/Modules/Roots.bas"
+        file_paths = ["src/Modules/Roots.bas"]
+        source_files = []
+        for file in file_paths:
+            file_cov = self.file_coverage(file)
+            source_file.append([file_cov])
+        commit_sha = os.environ.get('GITHUB_SHA')
+        assert commit_sha is not None
+        
+        report = {
+            "repo_token": os.environ['COVERALLS_REPO_TOKEN'],
+            "service_name": "manual",
+            "service_job_id": self.git.job_id,
+            "source_files": source_files,
+            "git": self.git.repo()
+        }
+        print(report)
+
+    def file_coverage(self: T, file_path: str) -> dict:
         with open(file_path, 'r') as f:
             line_count = sum(1 for line in f)
-        with open(file_path, 'r') as f:
-            source_code = f.read()
+        
         coverage = [None] * line_count
         for i in range(line_count):
             line_num = i + 1
@@ -27,28 +43,12 @@ class Coveralls(Coverage):
                 coverage[i] = 1
             else:
                 coverage[i] = 0
+        with open(file_path, 'r') as f:
+            source_code = f.read()
         digest = hashlib.md5(source_code.encode('utf-8')).hexdigest()
-        commit_sha = os.environ.get('GITHUB_SHA')
-        assert commit_sha is not None
-
-        # 3. Remote URL from git config
-        remote_url = subprocess.check_output(
-            ["git", "config", "--get", "remote.origin.url"],
-            text=True
-        ).strip()
-        report = {
-            "repo_token": os.environ['COVERALLS_REPO_TOKEN'],
-            "service_name": "manual",
-            "service_job_id": self.git.job_id,
-            "source_files": [
-                {
-                    "name": file_path,
-                    "source_digest": digest,
-                    "source": source_code,
-                    "coverage": [1, None, None, None, None, None, None, None,
+        return {
+            "name": file_path,
+            "source_digest": digest,
+            "coverage": [1, None, None, None, None, None, None, None,
                                  None, None, None, 1, 1, 1],
-                }
-            ],
-            "git": self.git.repo()
         }
-        print(report)
