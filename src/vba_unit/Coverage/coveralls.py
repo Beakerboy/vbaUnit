e163c7c1eee554e811378415bb243d72eade7e60
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-06 13:51:24 -0400

    Update coveralls.py

--- src/vba_unit/Coverage/coveralls.py
+++ src/vba_unit/Coverage/coveralls.py
@@ -3,7 +3,7 @@ import os
 from typing import TypeVar
 from vba_unit.Coverage.coverage import Coverage
 from vba_unit.Coverage.git_repo import GitRepo
-from vba_unit.Interpreter.coverage_table import VbaUnitModDef
+from vba_unit.Interpreter.coverage_table import CoverageTable, VbaUnitModDef
 
 
 T = TypeVar('T', bound='Coveralls')
@@ -13,6 +13,7 @@ class Coveralls(Coverage):
     def __init__(self: T) -> None:
         self.endpoint = "https://coveralls.io/api/v1/jobs"
         self.git: GitRepo
+        self.table: CoverageTable
 
     def generate_report(self: T) -> dict:
         source_files = []
