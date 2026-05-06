1107c68f464b5d7730e70ce75e4c2e18cc38137f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-06 10:06:07 -0400

    Update coverage.py

--- src/vba_unit/Coverage/coverage.py
+++ src/vba_unit/Coverage/coverage.py
@@ -1,12 +1,16 @@
 import json
 import requests
 from typing import TypeVar
+from vba_unit.Coverage.coverage_table import CoverageTable
 
 
 T = TypeVar('T', bound='Coverage')
 
 
 class Coverage():
+    def __init__(self: T) -> None:
+        self.table: CoverageTable
+
     def submit_report(self: T) -> str:
         report = self.generate_report()
         response = requests.post(
