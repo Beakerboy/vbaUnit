d0e4c8961d4983c0d015ccaf2190b2587f401545
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-06 09:55:01 -0400

    Create cov_fact.py

---
+++ src/vba_unit/Coverage/cov_fact.py
@@ -0,0 +1,8 @@
+from .coverage import Coverage
+from .git_repo import GitRepo
+
+class CovFact():
+    @staticmethod
+    def provider(name: str) -> Coverage:
+        if name = coveralls:
+            return Coveralls(GitRepo())
