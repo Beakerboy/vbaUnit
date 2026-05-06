e590a01ff980acf0e1f13b6442e17fa46c565844
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-06 15:37:14 -0400

    Update test_coverage.py

--- tests/Unit/test_coverage.py
+++ tests/Unit/test_coverage.py
@@ -3,6 +3,6 @@ from vba_unit.Coverage.coverage import Coverage
 
 
 def test_constructor() -> None:
-      obj = Coverage()
-      with pytest.raises(Exception):
-          obj.generate_report()
+    obj = Coverage()
+    with pytest.raises(Exception):
+        obj.generate_report()
