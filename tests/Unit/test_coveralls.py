b6dfe31ce7657bf87ac41855c1a4df77d77800f7
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-06 13:56:45 -0400

    Create test_coveralls.py

---
+++ tests/Unit/test_coveralls.py
@@ -0,0 +1,36 @@
+from vba_unit.Coverage.coveralls import Coveralls
+
+
+def test_constructor() -> None:
+      obj = Coveralls()
+      assert obj.endpoint = "https://coveralls.io/api/v1/jobs"
+
+
+report = {
+    'repo_token': 'secretsecretsecret',
+    'service_name': 'manual',
+    'service_job_id': '25396149145',
+    'source_files': [
+        {
+            'name': 'src/Modules/Roots.bas',
+            'source_digest': '72a03368f06a7905c304c52068d77755',
+            'coverage': [1, None, None, None, None, None, None, None, None, None, None, 1, 1, 1]
+        }
+    ],
+    'git': {
+        'head': {
+            'id': '036c36dfac1d00cb37b6510fc423641cda7b1f08',
+            'author_name': 'John Doe',
+            'author_email': 'me@me.com',
+            'committer_name': 'GitHub',
+            'committer_email': 'noreply@github.com',
+            'message': 'Merge 44874a067b515009088d34a49bc1cb8fbb483556 into c3b85fff65ec7496983d013cdc1057ccb54a3b3f'
+        },
+        'branch': 'PR-6/merge',
+        'remotes': [
+            {'name': 'origin',
+             'url': 'https://github.com/Beakerboy/VBA-Projects'
+            }
+        ]
+    }
+}
