16fa3b6f50f2be8b54922138dc8eb9cddbf41670
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-06 12:09:13 -0400

    Refactor coverage initialization for module tracking

--- src/vba_unit/Interpreter/vba_unit_listener.py
+++ src/vba_unit/Interpreter/vba_unit_listener.py
@@ -16,8 +16,8 @@ class VbaUnitListener(VbaListener):
         token_stream = self.parser.getInputStream()
         eof_token = token_stream.get(token_stream.size - 1)
         total_lines = eof_token.line
-        mod = self.table[project]["modules"][self.module_name.lower()]
-        mod["coverage"] = [0] * total_lines
+        mods = self.table[self.project_name]["modules"]
+        mods[self.module_name.lower()]["coverage"] = [0] * total_lines
 
     def enterFunctionDeclaration(                                  # noqa: N802
             self: T,
