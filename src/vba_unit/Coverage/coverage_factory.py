73f7de7a02c91e29516ebcf297fbffc702fbfd3b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-06 09:57:58 -0400

    Update coverage_factory.py

--- src/vba_unit/Coverage/coverage_factory.py
+++ src/vba_unit/Coverage/coverage_factory.py
@@ -1,8 +1,7 @@
 from .coverage import Coverage
-from .git_repo import GitRepo
 
 class CovFact():
     @staticmethod
     def provider(name: str) -> Coverage:
         if name == "coveralls":
-            return Coveralls(GitRepo())
+            return Coveralls()
