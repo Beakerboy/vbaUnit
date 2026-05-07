35c90e3caa373e992f57f980a18e3dc7be03e498
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-07 14:22:27 -0400

    Update vba_unit_listener.py

--- src/vba_unit/Interpreter/vba_unit_listener.py
+++ src/vba_unit/Interpreter/vba_unit_listener.py
@@ -55,7 +55,7 @@ class VbaUnitListener(VbaListener):
             if (ctx.start.column == 0 or
                     in_str.get(ctx.start.tokenIndex - 1).column == 0):
                 # Comments cannot be the first token in a file, so
-                # tokenIndex - 1 cannot be zero
+                # tokenIndex - 1 cannot be less than zero
                 line_num = ctx.start.line
                 mods = self.table.definitions[self.project_name]["modules"]
                 mods[self.module_name.lower()]["coverage"][line_num] = None
