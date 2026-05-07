fb2a2731fd77dade02145a814c8e02aa2e367b18
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-06 21:01:37 -0400

    Update test_github.py

--- tests/Unit/test_github.py
+++ tests/Unit/test_github.py
@@ -6,5 +6,5 @@ from vba_unit.Coverage.github import Github
 @mock.patch.dict(os.environ, {"GITHUB_RUN_ID": "123"})
 def test_construct() -> None:
     github = Github()
-    assert github.job_id = "123"
+    assert github.job_id == "123"
     
