7e6bd1e45e5fe140e45a14e056bd67f3bca61b9a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-07 11:14:04 -0400

    Enhance exception handling in test_constructor
    Assert specific exception message in test.

--- tests/Unit/test_coverage.py
+++ tests/Unit/test_coverage.py
@@ -4,5 +4,6 @@ from vba_unit.Coverage.coverage import Coverage
 
 def test_constructor() -> None:
     obj = Coverage()
-    with pytest.raises(Exception):
+    with pytest.raises(Exception) as e:
         obj.generate_report()
+    assert str(e.value) == "Must be implemented by an extending class."
