9027d7d6e974c60386dbfe22ac6b32f598fd45e3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-07 07:52:27 -0400

    Update test_github.py

--- tests/Unit/test_github.py
+++ tests/Unit/test_github.py
@@ -6,12 +6,18 @@ from vba_unit.Coverage.github import Github
 
 @mock.patch.dict(os.environ, {
     "GITHUB_RUN_ID": "25396149145",
-    "GITHUB_SHA": "123",
-    "GITHUB_REF": "master"
+    "GITHUB_SHA": "036c36dfac1d00cb37b6510fc423641cda7b1f08",
+    "GITHUB_REF": "refs/pull/6/merge"
 })
 @patch("subprocess.check_output")
 def test_construct(mock_check_output: str) -> None:
-    mock_check_output.return_value = "John Doe\nme@me.com\nGitHub\nnoreply@github.com\ncommit message"
+    mock_check_output.return_value = ["John Doe\nme@me.com\nGitHub\nnoreply@github.com\ncommit message", 'https://github.com/Beakerboy/FooProject']
     github = Github()
     assert github.job_id == "25396149145"
     assert github.author_name == John Doe
+    assert github.author_email == me@me.com
+    assert github.committer_name == Github
+    assert github.committer_email == noreply@github.com
+    assert github.message == "commit message"
+    assert self.remote_url == 'https://github.com/Beakerboy/vbaUnit'
+    assert github.branch == "PR-6/merge"
