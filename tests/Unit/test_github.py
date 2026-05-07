c5d6e7a984bb78cc749f0fd765bfa4106a0f40c6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-06 20:59:56 -0400

    Create test for Github class with mocked environment
    Add unit test for Github class construction.

---
+++ tests/Unit/test_github.py
@@ -0,0 +1,10 @@
+import os
+from unittest import mock
+from vba_unit.Coverage.github import Github
+
+
+@mock.patch.dict(os.environ, {"GITHUB_RUN_ID": "123"})
+def test_construct() -> None:
+    github = Github()
+    assert github.job_id = "123"
+    
