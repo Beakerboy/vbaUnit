458ff2f21e37039626d7b12efea346d2be291cd8
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-07 09:08:29 -0400

    Update test_github.py

--- tests/Unit/test_github.py
+++ tests/Unit/test_github.py
@@ -27,13 +27,13 @@ def test_construct(mock_check_output: str) -> None:
     expected_calls = [
             [
                 ["git", "log", "-1", "--pretty=format:%an%n%ae%n%cn%n%ce%n%s", "036c36dfac1d00cb37b6510fc423641cda7b1f08"],
-                "text=True"
+                "text=True",
+                ""
             ],
             [
                 ["git", "config", "--get", "remote.origin.url"],
                 "text=True"
             ],
-            []
         ]
     mock_check_output.assert_has_calls(expected_calls)
     expected = {
