f015194cdf181c30dd2e8ea62716548e4585dc9b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-07 11:10:45 -0400

    Create test_coverage_factory.py

---
+++ tests/Unit/test_coverage_factory.py
@@ -0,0 +1,14 @@
+import pytest
+from vba_unit.Coverage.coverage_factory import CovFact
+from vba_unit.Coverage.coveralls import Coveralls
+
+
+def test_get_coveralls() -> None:
+    co = CovFact.provider("github")
+    assert isinstance(co, Coveralls)
+
+
+def test_get_except() -> None:
+    with pytest.raises(Exception) as e:
+        CovFact.provider("foo")
+    assert str(e.value) == "Unknown coverage provider."
