810b57cc3d5d0bfc09eb63c36a0a64f5c741b9f4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-07 07:42:07 -0400

    Update test_github.py

--- tests/Unit/test_github.py
+++ tests/Unit/test_github.py
@@ -11,7 +11,7 @@ from vba_unit.Coverage.github import Github
 })
 @patch("subprocess.check_output")
 def test_construct(mock_check_output: str) -> None:
-    mock_check_output.return_value = "John Doe\nme@me.com\nGitHub\nnoreply@github.com\nmessage"
+    mock_check_output.return_value = "John Doe\nme@me.com\nGitHub\nnoreply@github.com\ncommit message"
     github = Github()
     assert github.job_id == "25396149145"
     assert github.author_name == John Doe
