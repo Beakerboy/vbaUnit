dead9d1d5ca99b0bd7792c3ca1b949e180b19224
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-07 07:41:44 -0400

    Update test_github.py

--- tests/Unit/test_github.py
+++ tests/Unit/test_github.py
@@ -9,14 +9,9 @@ from vba_unit.Coverage.github import Github
     "GITHUB_SHA": "123",
     "GITHUB_REF": "master"
 })
-def test_construct() -> None:
+@patch("subprocess.check_output")
+def test_construct(mock_check_output: str) -> None:
+    mock_check_output.return_value = "John Doe\nme@me.com\nGitHub\nnoreply@github.com\nmessage"
     github = Github()
     assert github.job_id == "25396149145"
-    
-def test_subprocess() -> None:
-    commit_sha = os.environ.get('GITHUB_SHA', '')
-    fmt = "%an%n%ae%n%cn%n%ce%n%s"
-    message = subprocess.check_output(
-            ["git", "log", "-1", f"--pretty=format:{fmt}", commit_sha],
-            text=True)
-    assert message == ""
+    assert github.author_name == John Doe
