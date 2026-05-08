832e3549f1a04026e077493b055355e9524024db
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-08 10:41:08 -0400

    Update vba_unit_visitor.py

--- src/vba_unit/Interpreter/vba_unit_visitor.py
+++ src/vba_unit/Interpreter/vba_unit_visitor.py
@@ -31,7 +31,8 @@ class VbaUnitVisitor(VbaVisitor):
                         self.context_changed = False
                         name = self.context[0]
                         mods = self.table.definitions[name]["modules"]
-                        coverage = mods[self.context[1]]["coverage"]
+                        extras = mods[self.context[1]]["extras"]["vba_unit"]
+                        coverage = extras["coverage"]
                         coverage[line_num - 1] += 1
         # Call the original visit to continue traversal
         return super().visit(tree)
@@ -45,7 +46,7 @@ class VbaUnitVisitor(VbaVisitor):
         # is there a way to prohibit it from being touched...does it matter?
         line_num = ctx.stop.line
         mods = self.table.definitions[self.context[0]]["modules"]
-        coverage = mods[self.context[1]]["coverage"]
+        coverage = mods[self.context[1]]["extras"]["vba_unit"]["coverage"]
         coverage[line_num - 1] += 1
 
         return super().visitFunctionDeclaration(ctx)
