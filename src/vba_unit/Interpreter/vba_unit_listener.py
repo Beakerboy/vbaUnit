0f1df650b0cc319050a1f59f3d52b600322a491b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-08 11:39:53 -0400

    Update vba_unit_listener.py

--- src/vba_unit/Interpreter/vba_unit_listener.py
+++ src/vba_unit/Interpreter/vba_unit_listener.py
@@ -1,3 +1,4 @@
+from antlr4 import CommonTokenStream
 from antlr4_vba.vbaParser import vbaParser as Parser
 from antlr4_vba.vbaLexer import vbaLexer as Lexer
 from pyvba_interpreter.symbol_table import SymbolTable
@@ -23,7 +24,7 @@ class VbaUnitListener(VbaListener):
             self: T,
             ctx: Parser.ProceduralModuleHeaderContext) -> None:
         super().enterProceduralModuleHeader(ctx)
-        token_stream = self.parser.getInputStream()
+        token_stream: CommonTokenStream = self.parser.getInputStream()
         token_stream.fill()
         eof_token = token_stream.get(len(token_stream.tokens) - 1)
         total_lines = eof_token.line
