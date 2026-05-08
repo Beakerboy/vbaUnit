d3c180e03dafb74f383d762df2a9fa9820fe0109
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-08 10:29:54 -0400

    Refactor coverage_table.py to use TypedDict

--- src/vba_unit/Interpreter/coverage_table.py
+++ src/vba_unit/Interpreter/coverage_table.py
@@ -1,22 +1,11 @@
-from pyvba_interpreter.symbol_table import (
-    FunctionDefinition, ModuleDefinition, ProjectDefinition, SymbolTable
-)
+from pyvba_interpreter.symbol_table import SymbolTable
+from typing import TypedDict
 
 
-class VbaUnitFuncDef(FunctionDefinition):
-    visited: bool
-    start_end_lines: tuple[int, int]
-
-
-class VbaUnitModDef(ModuleDefinition):
+class VbaUnitModuleExtras(TypedDict):
     path: str                              # The file path
     cover: bool                            # Track coverage on this file?
     coverage: list[None | int]             # lines covered
-    functions: dict[str, VbaUnitFuncDef]   # {@inheritDoc}
-
-
-class VbaUnitProjDef(ProjectDefinition):
-    modules: dict[str, VbaUnitModDef]
 
 
 class CoverageTable(SymbolTable):
