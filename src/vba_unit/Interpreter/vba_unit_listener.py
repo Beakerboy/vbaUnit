0260d3ec8d7d9cbc04e073f4465e6bf57f1b8430
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-08 11:47:00 -0400

    Update vba_unit_listener.py

--- src/vba_unit/Interpreter/vba_unit_listener.py
+++ src/vba_unit/Interpreter/vba_unit_listener.py
@@ -24,7 +24,7 @@ class VbaUnitListener(VbaListener):
             self: T,
             ctx: Parser.ProceduralModuleHeaderContext) -> None:
         super().enterProceduralModuleHeader(ctx)
-        token_stream = self.parser.getTokenStream()
+        token_stream = cast(CommonTokenStream, self.parser.getTokenStream())
         token_stream.fill()
         eof_token = token_stream.get(len(token_stream.tokens) - 1)
         total_lines = eof_token.line
@@ -48,7 +48,7 @@ class VbaUnitListener(VbaListener):
         super().enterCommentBody(ctx)
         # Check if the token starts at column 1 or if the token before this is
         # a wsc and it starts at column 1
-        in_str: CommonTokenStream = self.parser.getInputStream()
+        in_str = cast(CommonTokenStream, self.parser.getTokenStream())
         if ctx.start is not None and ctx.start.tokenIndex is not None:
             if (ctx.start.column == 0 or
                     in_str.get(ctx.start.tokenIndex - 1).column == 0):
@@ -63,7 +63,7 @@ class VbaUnitListener(VbaListener):
             self: T,
             ctx: Parser.EndOfLineContext) -> None:
         super().enterEndOfLine(ctx)
-        in_str: CommonTokenStream = self.parser.getInputStream()
+        in_str = cast(CommonTokenStream, self.parser.getTokenStream())
         if ctx.start is not None:
             tok_ind = ctx.start.tokenIndex
             is_wsc = in_str.get(tok_ind - 1).type == Lexer.WS
