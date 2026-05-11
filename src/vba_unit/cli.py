48e9844eae1e97eb56ab2bd6fcb14a8f3c65f2e4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-11 14:27:03 -0400

    Import vba_stdlib.api if 'vba' library is specified

--- src/vba_unit/cli.py
+++ src/vba_unit/cli.py
@@ -59,6 +59,9 @@ def main() -> None:
 
     args = parser.parse_args()
     table = SymbolTable()
+    if "vba" in args.libraries:
+        from vba_stdlib.api import api
+        table.library_descriptions["vba"] = api
     run_tests(args.src, args.tests, args.project, table)
 
     # Submit Coverage
